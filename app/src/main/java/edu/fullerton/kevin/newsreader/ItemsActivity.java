4d703fd8443f52cca548da7b4fea03a59d25e1bf
Kevin Truong <kevintruong@MacBook-Pro-de-Kevin.local>
2018-07-21 19:42:53 -0700

    Download Feed

--- app/src/main/java/edu/fullerton/kevin/newsreader/ItemsActivity.java
+++ app/src/main/java/edu/fullerton/kevin/newsreader/ItemsActivity.java
@@ -5,7 +5,13 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.widget.Toast;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 
 /**
  * Created by Kevin on 7/17/2018.
@@ -28,7 +34,25 @@ public class ItemsActivity extends Activity{
             String urlString = strings[0];
 
             //download the feed and write it to a file
+            final String FILENAME = "news_feed.xml";
+            try{
+                URL url = new URL("http://rss.cnn.com/rss/cnn_tech.rss");
+                InputStream in = url.openStream();
 
+                FileOutputStream out = openFileOutput(FILENAME, MODE_PRIVATE);
+
+                byte[] buffer = new byte[1024];
+                int bytesRead = in.read(buffer);
+                while(bytesRead != -1){
+                    out.write(buffer, 0, bytesRead);
+                    bytesRead = in.read(buffer);
+                }
+                out.close();
+                in.close();
+            }
+            catch (IOException e){
+                Log.e("NewsReader", e.toString());
+            }
             return "Feed downloaded";
         }
 
@@ -38,4 +62,6 @@ public class ItemsActivity extends Activity{
             Toast.makeText(ItemsActivity.this, s, Toast.LENGTH_SHORT).show();
         }
     }
+
+
 }
